7ea3d1e1f15e9207cd471e7bfe3eab0a171b25c1
foryouwithlove <harsheyy2oo5@gmail.com>
2026-09-04 08:39:49 +0530

    Update kadhal.js

--- assets/kadhal.js
+++ assets/kadhal.js
@@ -3,7 +3,6 @@ const reasons=document.querySelectorAll(".reason");
 reasons.forEach(reason=>{
 reason.addEventListener("click",()=>{
 reason.classList.add("clicked");
-
 setTimeout(()=>{
 reason.classList.remove("clicked");
 },200);
